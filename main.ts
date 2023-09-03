32675db5f549c45df9ea11bbd42209f69ff2e5c7
Simon Warta <simon@warta.it>
2023-09-03 12:00:03 +0200

    Improve logging

--- main.ts
+++ main.ts
@@ -84,9 +84,17 @@ if (import.meta.main) {
   const client = await SigningCosmWasmClient.createWithSigner(tmClient, wallet, {
     gasPrice: GasPrice.fromString(config.gasPrice),
   });
+
+  // Bot info
   const botAddress = firstAccount.address;
   console.log(`Bot address: ${botAddress}`);
   console.log(`Group: ${group(botAddress)}`);
+  try {
+    const balance = await client.getBalance(botAddress, config.denom);
+    console.log(`Balance: ${printableCoin(balance)}`);
+  } catch (error: unknown) {
+    console.warn(`Error getting bot balance: ${error}`);
+  }
 
   // Needed in case an error happened to ensure sequence is in sync
   // with chain
@@ -107,7 +115,7 @@ if (import.meta.main) {
 
   const moniker = config.moniker;
   if (moniker) {
-    console.info("Registering this bot ...");
+    console.info(`Registering bot '${moniker}'...`);
     const fee = calculateFee(gasLimitRegister, config.gasPrice);
     await client.execute(
       botAddress,
@@ -122,8 +130,12 @@ if (import.meta.main) {
   await Promise.all([
     sleep(500), // the min waiting time
     (async function () {
-      const listed = await queryIsAllowlisted(client, drandAddress, botAddress);
-      console.info(`Bot allowlisted for rewards: ${listed}`);
+      try {
+        const listed = await queryIsAllowlisted(client, drandAddress, botAddress);
+        console.info(`Bot allowlisted for rewards: ${listed}`);
+      } catch (error: unknown) {
+        console.warn(`Query error: ${error}`);
+      }
     })(),
   ]);
 
